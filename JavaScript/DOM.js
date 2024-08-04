fbeb4e15e22e7aec9b985e707e1ddf6d8b0ba9bc
Nsikak Akpan <builtbykaksy@gmail.com>
2024-08-04 01:53:26 +0100

    intro to DOM manipulation

---
+++ JavaScript/DOM.js
@@ -0,0 +1,11 @@
+document.write("<h1>Happy Hour!</h1>")
+
+document.getElementById('demo').innerHTML = 'Tinubu' 
+document.getElementById('demo').style.color ='green'
+
+document.getElementById('box').style.backgroundColor='blue'
+document.getElementById('box').style.width='200px'
+document.getElementById('box').style.height='200px'
+document.getElementById('heading').innerHTML='My Box'
+document.getElementById('heading').style.color='white'
+
