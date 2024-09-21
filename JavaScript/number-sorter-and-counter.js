779420e9b1590b996ceaf1601553eed1e77526f1
Nsikak Akpan <builtbykaksy@gmail.com>
2024-09-21 15:50:59 +0100

    new code

---
+++ JavaScript/number-sorter-and-counter.js
@@ -0,0 +1,35 @@
+let randomNumbers = [];
+let count = 0;
+
+for(let k = 0; k <=15; k++){
+    let randomNumber = Math.round(Math.random() * 50);
+    randomNumbers.push(randomNumber)
+}
+
+console.log(randomNumbers);
+
+for(let i = 0; i < randomNumbers.length; i++){
+    // Optimize: Reduce the number of iterations of the inner loop
+    for(let j = 0; j < randomNumbers.length - i - 1; j++) {
+        if(randomNumbers[j] > randomNumbers[j + 1]){
+            let biggerNumber = randomNumbers[j];
+            let smallerNumber = randomNumbers[j + 1];
+            randomNumbers[j] = smallerNumber;
+            randomNumbers[j + 1] = biggerNumber;
+        }
+    }    
+}
+
+// randomNumbers.sort((a, b) => a-b);
+
+// randomNumbers.sort((a, b) => {
+//     return a - b;
+// })
+
+
+// randomNumbers.sort(function(a, b) {
+//     return a - b 
+// })
+// randomNumbers[0] = 67;
+
+console.log(randomNumbers);
---
+++ JavaScript/objects.js
@@ -0,0 +1,187 @@
+// Objects in JavaScript are collections of key-value pairs. 
+// Each value can be a string, number, array, another object, or a function (method). 
+// Objects are used to store and manage data in a structured way.
+
+
+// There are two main ways to create an object:
+
+// 1.) Object Literal Notation:
+const person = {
+    name: 'John',         
+    age: 30,              
+    profession: 'Engineer',
+    greet: function() {    
+      console.log('Hello, my name is ' + this.name);
+    },
+    drive: (carname) => {
+        console.log(carname + ' Vroooooooooooooom...');
+    }
+  };
+
+
+// 2.)  Using new Object() Syntax:
+ 
+  const car = new Object();
+  car.brand = 'Toyota';
+  car.model = 'Corolla';
+  car.year = 2020;
+  
+  console.log(car); 
+
+
+
+
+
+//   Accessing Object Properties
+//   You can access object properties in two ways:
+  
+//   Dot Notation:
+  console.log(person.name);  // Output: John
+
+// Bracket Notation (useful when keys are dynamic or have spaces):
+  console.log(person['age']);  // Output: 30
+
+
+//   Methods
+//   A method is a function that is a property of an object. 
+//   In the above example, greet is a method of the person object.
+person.drive('Toyota');
+  
+
+
+// Destructuring Objects
+// Destructuring allows you to unpack values from objects into distinct variables.
+
+const { name, age } = person;
+console.log(name);  // Output: John
+console.log(age);   // Output: 30
+
+
+// Nested Object Destructuring
+// You can destructure nested objects too:
+const student = {
+    name: 'Tom',
+    scores: {
+      math: 90,
+      english: 85
+    }
+  };
+  
+  const { scores: { math, english } } = student;
+  
+  console.log(math);    // Outputs: 90
+  console.log(english); // Outputs: 85
+  
+
+
+//  Spread Operator (...)
+// The spread operator (...) allows you to copy or merge objects and arrays, as well as to unpack elements.
+
+// Copying an Object
+const person2 = { ...person, city: 'New York' };
+console.log(person2);
+
+// Copying an Array
+const cars = ['Toyota', 'Ford', 'BMW'];
+const cars2 = [...cars, 'Honda'];
+
+// Merging Objects
+// You can merge multiple objects using the spread operator:
+const obj1 = { x: 1, y: 2 };
+const obj2 = { y: 5, z: 3 };
+
+const merged = { ...obj1, ...obj2 }; // `y` will be overwritten by obj2's `y`
+console.log(merged);  // Outputs: { x: 1, y: 5, z: 3 }
+
+
+
+// Adding or Overwriting Properties in an Object
+// You can add or modify properties while spreading:
+
+const person3 = { name: 'John', age: 30 };
+const updatedPerson = { ...person3, city: 'lagos', age: 35 };
+
+console.log(updatedPerson);
+
+
+// Deleting Properties in an Object
+const person4 = { name: 'John', age: 30, city: 'New York' };
+const deletedPerson = { ...person4 };
+delete deletedPerson.city;
+
+console.log(deletedPerson);
+
+
+
+// Object Methods
+// There are built-in methods to manipulate objects.
+
+// Object.keys()
+// Returns an array of the object’s keys.
+const citizen = { name: 'John', age: 30 };
+console.log(Object.keys(citizen));
+
+// Object.values()
+// Returns an array of the object’s values.
+console.log(Object.values(citizen));
+
+
+// Object.entries()
+// Returns an array of key-value pairs.
+console.log(Object.entries(citizen));
+
+
+// this Keyword
+// In JavaScript, this refers to the current object the code is being written in. 
+// Inside an object method, this refers to the object.
+const user = {
+    name: 'Emma',
+    greet: function() {
+      console.log('Hello, ' + this.name);
+    }
+  };
+  
+  user.greet();
+
+
+//   Object Shorthand Syntax
+//   When creating objects, if the key and variable name are the same, you can use shorthand syntax:
+const myName = 'Mark';
+const myAge = 22;
+
+const myDetails = { myName, myAge };
+console.log(myDetails);
+
+
+// Object Property Computation
+// You can dynamically define property keys using square brackets.
+const keyName = 'color';
+const car = {
+  brand: 'BMW',
+  [keyName]: 'red'
+};
+
+console.log(car);
+
+
+// Object freeze() and seal()
+
+// Object.freeze(): Prevents modification of properties in the object.
+const obj = { a: 1 };
+Object.freeze(obj);
+obj.a = 2;  // No effect, because the object is frozen
+console.log(obj);
+
+// Object.seal(): Allows modification of existing properties but prevents new properties from being added.
+const obj = { a: 1 };
+Object.seal(obj);
+obj.a = 2;  // Allowed
+obj.b = 3;  // Not allowed
+console.log(obj);
+
+
+
+//Class Work
+// 1.) create an Object called student
+// 2.) give it a name property and assign a string value to it
+// 3.) give it a grades property and assign an array of 7 numbers to it
